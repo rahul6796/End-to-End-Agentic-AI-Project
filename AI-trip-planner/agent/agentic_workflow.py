5f2b2c11fa43c1324d06d657d2927ad7f58a0348
Rahul Parjapati <60359760+rahul6796@users.noreply.github.com>
2025-06-30 19:27:59 +0530

    agentic_workflow
    i have build the graph and agent function and prompt for this project

--- AI-trip-planner/agent/agentic_workflow.py
+++ AI-trip-planner/agent/agentic_workflow.py
@@ -14,13 +14,38 @@ from langgraph.prebuilt import ToolNode, tool_condition
 class GraphBuilder():
 
     def __init__(self):
-        pass
+        
+        self.tools= []
 
-    def agent_function(self):
-        pass
+        self.system_prompt = SYSTEM_PROMPT
+
+
+    def agent_function(self, state: MessageState):
+        """
+        main agent function
+        """
+        user_question = state['message']
+        input_question = [self.system_prompt] + user_question
+        response = self.llm_with_tools.invoke(input_question)
+        return {'message': response}
+
+
 
     def build_graph(self):
-        pass
+        """
+        In this method we desinged the architecture of graph.
+
+        """
+
+        graph_builder = StartGraph(MessageState)
+        graph_builder.add_node('agent', self.agent_function)
+        graph_builder.add_node('tools', ToolNode(tools = self.tools))
+        graph_builder.add_edge(START, 'agent')
+        graph_builder.add_conditional_edge('agent', tools_condition)
+        graph_builder.add_edge('tools', 'agent')
+        graph_builder.add_edge('agent', END)
+        self.graph = graph_builder.complie()
+        return self.graph
 
     def __call__(self):
-        pass
+        return self.build_graph()
